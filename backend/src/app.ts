b2b7a7e2c9868c265a181f24b066eb219d4e4735
TimUnterberger <timuntersberger2@gmail.com>
2019-08-26 21:39:46 +0300

    Add morgan with output format of "dev" as express http request logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express"
+/* Middlewares */
+import morgan from "morgan"
+/* Routes */
 import healthCheck from "./routes/healthCheck"
 
 const app = express()
+
+app.use(morgan("dev", {immediate: true}))
 
 app.use("/healthCheck", healthCheck)
 
